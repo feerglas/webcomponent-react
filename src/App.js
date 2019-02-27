56ba667f711e9d4ee3d213c6dc73b6073d85fc17
Yves Tscherry <yves.tscherry@unic.com>
2019-02-27 08:57:36 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DateTime from './Components/DateTime/DateTime';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			date: ''
+		};
+	}
+
+	componentDidMount() {
+		this.setState({
+			date: '1970-03-10T00:00:00.000Z'
+		});
+	}
+
+	update = () => {
+		this.setState({
+			date: new Date()
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<DateTime date={this.state.date} />
+				<button onClick={this.update.bind(this)}>update</button>
+			</div>
+		);
+	}
 }
 
 export default App;
