f5fcac3d7051a709a277fac7bf404165cdf68eaa
Yves Tscherry <yves.tscherry@unic.com>
2019-03-09 16:14:04 +0100

    Add body styles, remove default webfonts config

--- src/App.js
+++ src/App.js
@@ -32,47 +32,12 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<sbb-webfonts
-					config='[
-					{
-						"version": "1_6_subset"
-					},
-					{
-						"styles": [
-						{
-							"name": "Roman",
-							"include": true,
-							"display": "fallback"
-						},
-						{
-							"name": "Bold",
-							"include": true,
-							"display": "swap"
-						},
-						{
-							"name": "Light",
-							"include": true,
-							"display": "swap"
-						},
-						{
-							"name": "Ultralight",
-							"include": true,
-							"display": "swap"
-						},
-						{
-							"name": "Thin",
-							"include": true,
-							"display": "swap"
-						}
-						]
-					}
-					]'
-				></sbb-webfonts>
+				<sbb-webfonts></sbb-webfonts>
 				<sbb-header language={this.state.language} items='{"de": [{ "title": "Fahrplan", "link": "#"}, { "title": "Abos & Billete", "link": "#"}, { "title": "Bahnhof & Services", "link": "#"}, { "title": "Geschäftskunden", "link": "#"}, { "title": "Freizeit & Ferien", "link": "#"}], "fr": [{ "title": "Horaire", "link": "#"}, { "title": "Abonnements et billets", "link": "#"}, { "title": "Gare et servicesa", "link": "#"}, { "title": "Clientèle commerciale", "link": "#"}, { "title": "Loisirs et vacances", "link": "#"}], "en": [{ "title": "Timetable", "link": "#"}, { "title": "Travelcards & tickets", "link": "#"}, { "title": "Station & services", "link": "#"}, { "title": "Business customers", "link": "#"}, { "title": "Leisure & holidays", "link": "#"}], "it": [{ "title": "Orario", "link": "#"}, { "title": "Abonamenti e biglietti", "link": "#"}, { "title": "Stazione e servizi", "link": "#"}, { "title": "Clientela aziendale", "link": "#"}, { "title": "Tempo libero e vacanze", "link": "#"}]}'></sbb-header>
 				<sbb-timetable-search></sbb-timetable-search>
 				<sbb-timetable-results></sbb-timetable-results>
 				<sbb-homepage-main-teaser></sbb-homepage-main-teaser>
-				<sbb-footer uselinks='false' ref={this.footerRef}>test</sbb-footer>
+				<sbb-footer language='de' uselinks='false' ref={this.footerRef}>test</sbb-footer>
 			</div>
 		);
 	}
