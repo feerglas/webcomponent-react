06652281886db652427edcc84171107565fcf9b3
Yves Tscherry <yves.tscherry@unic.com>
2019-03-12 19:57:17 +0100

    Add service worker file and service worker support, add github pages deploy pipeline

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { defineCustomElements } from '@stauffacher/sbbpoc/dist/loader';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 defineCustomElements(window);
+
+serviceWorker.register();
